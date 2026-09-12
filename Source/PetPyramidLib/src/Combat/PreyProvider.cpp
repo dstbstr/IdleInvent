5df55fdc539de062b0b6f6737624bbc526c4c89d
Dustin Randall <durandal@microsoft.com>
2026-09-12 10:37:03 -0700

    Add prey rarity

--- Source/PetPyramidLib/src/Combat/PreyProvider.cpp
+++ Source/PetPyramidLib/src/Combat/PreyProvider.cpp
@@ -2,6 +2,9 @@
 #include "Pets/Pets/Pets.h"
 #include "Pets/Pets/PreyDetails.h"
 
+#include <DesignPatterns/ServiceLocator.h>
+#include <Utilities/IRandom.h>
+
 #include <array>
 
 namespace Pets::PreyProvider {
@@ -10,14 +13,36 @@ namespace Pets::PreyProvider {
             PetKind::Bloodhound, PetKind::Labrador, PetKind::Poodle, PetKind::Chihuahua, PetKind::HouseCat
         };
 
-        static size_t kindIndex = 0;
-        auto kind = Valid[kindIndex];
-        kindIndex = (kindIndex + 1) % Valid.size();
-        auto details = Details::GetPrey(kind);
+        struct Candidate {
+            const PreyDetails* Details{};
+            f64 CumulativeWeight{};
+        };
+
+        std::array<Candidate, Valid.size()> candidates{};
+        auto totalWeight = 0.0;
+
+        for(size_t i = 0; i < Valid.size(); i++) {
+            auto& details = Details::GetPrey(Valid[i]);
+            totalWeight += 1.0 / static_cast<f64>(details.Encounter.Rarity);
+            candidates[i] = {&details, totalWeight};
+        }
+
+        auto& rand = ServiceLocator::Get().GetRequired<IRandom>();
+        auto roll = rand.GetNextFloat() * static_cast<f32>(totalWeight);
+        auto* selected = candidates.back().Details;
+
+        for(auto candidate : candidates) {
+            if(roll < candidate.CumulativeWeight) {
+                selected = candidate.Details;
+                break;
+            }
+        }
+
+        const auto& details = *selected;
         return HuntCombatant{
             .ActionInterval = OneSecond,
             .Stats = PreyStats{
-                .Kind = kind,
+                .Kind = details.Kind,
                 .Battle = details.Battle,
                 .CurrentHp = details.Battle.MaxHp,
                 .FleeTime = details.Battle.FleeTime
